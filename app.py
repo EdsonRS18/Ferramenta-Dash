db21f7e7471002909704bf1fa65d296005300287
Edson Gomes <edson.bol.santos@hotmail.com>
2024-01-03 14:39:28 -0300

    ajustando

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 # Importação de bibliotecas necessárias
-from dash import Dash, html, dcc, Input, Output, State
-from dash.dependencies import Input, Output
+from dash import Dash, html, dcc, Input, Output
+import dash_bootstrap_components as dbc 
 from data_utils import load_data, create_graph
 import networkx as nx
-import dash_bootstrap_components as dbc 
-# Importação de funções personalizadas
-import malaria_layout 
 import layout
+import malaria_layout
 import callbacks
-
 
 external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css']
 
@@ -17,17 +14,16 @@ app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callbac
 # Carregar os dados
 df = load_data('teste.csv', nrows=15000)
 df['notificacoes_proprias'] = df.groupby('mun_noti')['notifications'].transform('sum')
-df['notificacoes_total'] = df.groupby('mun_noti')['notifications'].transform('sum')
+df['notificacoes_total'] = df['notificacoes_proprias'].groupby(df['mun_noti']).transform('sum')
 G = create_graph(df)
 pos = nx.get_node_attributes(G, 'pos')
 node_color = ['blue' if node in df['mun_noti'].values else 'green' for node in G.nodes()]
 
-# Definição do layout para a página principal
+# Definição dos layouts
 layout_principal = html.Div([
     layout.create_layout(df),  # Layout principal
 ])
 
-# Definição do layout para a página de malária
 pagina_malaria = html.Div([
     malaria_layout.create_malaria_layout(df),  # Página sobre malária
 ])
@@ -42,7 +38,6 @@ def alternar_paginas(n_clicks):
         return layout_principal
     else:
         return pagina_malaria
-  
 
 # Layout completo da aplicação com o botão de navegação
 app.layout = html.Div([
@@ -51,29 +46,27 @@ app.layout = html.Div([
     html.Div(id='page-content')
 ])
 
+# Callback para atualização dos componentes
 @app.callback(
-    [
-        Output('grafo-direcional', 'figure'),
-        Output('grafico-colunas', 'figure'),
-        Output('cidade-dropdown', 'value'),
-        Output('show-all-button', 'n_clicks')
-    ],
-    [
-        Input('cidade-dropdown', 'value'),
-        Input('grafo-direcional', 'clickData'),
-        Input('show-all-button', 'n_clicks'),
-        Input('hide-matching-municipality', 'value'),
-        Input('search-input', 'value')  # Novo input para o campo de busca
-    ]
+    Output('grafo-direcional', 'figure'),
+    Output('grafico-colunas', 'figure'),
+    Output('cidade-dropdown', 'value'),
+    Output('show-all-button', 'n_clicks'),
+    [Input('cidade-dropdown', 'value')],
+    [Input('grafo-direcional', 'clickData')],
+    [Input('show-all-button', 'n_clicks')],
+    [Input('hide-matching-municipality', 'value')],
+    [Input('search-input', 'value')]
 )
 
-
+    
 def update_graph_callback(selected_cidade, click_data, show_all_clicks, hide_matching_municipality, search_input):
-    # Garanta que o valor do campo de busca não seja None ou uma string vazia
-    if search_input is None or search_input == '':
+    # Lógica para processar os inputs e atualizar os componentes
+    # Esta função retorna os valores atualizados para os componentes especificados nos Outputs
+    if search_input in (None, ''):
         search_input = None
 
     return callbacks.update_graph_callback(selected_cidade, click_data, show_all_clicks, df, G, pos, hide_matching_municipality, search_input)
+
 if __name__ == '__main__':
     app.run_server(debug=True)
-
--- callbacks.py
+++ callbacks.py
@@ -1,36 +1,25 @@
-from dash import html, Input, Output, State
-import dash_bootstrap_components as dbc
-from dash import dcc
 import plotly.graph_objects as go
 from graph_utils import update_graph
 
-def update_graph_callback(selected_cidade, click_data, show_all_clicks, df, G, pos, hide_matching_municipality=None, search_input=None):
-    print(f"Selected city: {selected_cidade}")
-    print(f"Search input: {search_input}")  # Adicione essa linha para verificar o valor inserido no campo de busca
+def update_graph_callback(selected_city, click_data, show_all_clicks, df, G, pos, hide_matching_municipality=None, search_input=None):
+    print(f"Selected city: {selected_city}")
+    print(f"Search input: {search_input}")
 
-    # Verifique se o botão "Mostrar Todas as Cidades" foi clicado
-    if show_all_clicks is not None and show_all_clicks > 0:
-        selected_cidade = 'Todas'
+    if show_all_clicks and show_all_clicks > 0:
+        selected_city = 'Todas'
         show_all_clicks = 0
-    else:
-        if click_data and 'points' in click_data:
-            clicked_node_code = click_data['points'][0]['customdata']
-            selected_cidade = clicked_node_code
-    
-    if search_input:  # Verifique se há algo no campo de busca
-        # Filtre os dados com base no valor inserido no campo de busca
-        df_filtered = df[df['nome_noti'].str.contains(search_input, case=False, na=False)]
-    else:
-        df_filtered = df
-    
-    if 'hide' in hide_matching_municipality:
-        df_filtered = df_filtered[df_filtered['mun_noti'] != df_filtered['mun_infe']]
-    
-    # Atualize os gráficos com os dados filtrados
-    grafo_direcional = update_graph(selected_cidade, df_filtered, G, pos)
-    
+    elif click_data and 'points' in click_data:
+        clicked_node_code = click_data['points'][0]['customdata']
+        selected_city = clicked_node_code
 
-    if selected_cidade == 'Todas':
+    df_filtered = df[df['nome_noti'].str.contains(search_input, case=False, na=False)] if search_input else df
+
+    if hide_matching_municipality and 'hide' in hide_matching_municipality:
+        df_filtered = df_filtered[df_filtered['mun_noti'] != df_filtered['mun_infe']]
+
+    grafo_direcional = update_graph(selected_city, df_filtered, G, pos)
+
+    if selected_city == 'Todas':
         grafico_colunas = {
             'data': [],
             'layout': {
@@ -40,22 +29,16 @@ def update_graph_callback(selected_cidade, click_data, show_all_clicks, df, G, p
             }
         }
     else:
-        dados_grafico_colunas = df_filtered[df_filtered['mun_noti'] == selected_cidade].groupby('mun_infe').agg({'notifications': 'sum', 'nome_infe': 'first', 'mun_noti': 'first'}).reset_index()
+        dados_grafico_colunas = df_filtered[df_filtered['mun_noti'] == selected_city].groupby('mun_infe').agg(
+            {'notifications': 'sum', 'nome_infe': 'first', 'mun_noti': 'first'}
+        ).reset_index().sort_values(by='notifications', ascending=True).tail(10)
 
-        dados_grafico_colunas = dados_grafico_colunas.sort_values(by='notifications', ascending=True).tail(10)
+        dados_grafico_colunas['percentagem'] = (dados_grafico_colunas['notifications'] / df_filtered[df_filtered['mun_noti'] == selected_city]['notifications'].sum()) * 100
 
-        dados_grafico_colunas['percentagem'] = (dados_grafico_colunas['notifications'] / df_filtered[df_filtered['mun_noti'] == selected_cidade]['notifications'].sum()) * 100
+        filtered_df = df_filtered[df_filtered['mun_noti'] == selected_city]
+        total_notifications_mun_noti = filtered_df['notificacoes_total'].iloc[0] if not filtered_df.empty else 0
+        selected_city_name = filtered_df['nome_noti'].iloc[0] if not filtered_df.empty else "Selected Municipality"
 
-        filtered_df = df_filtered[df_filtered['mun_noti'] == selected_cidade]
-
-        if not filtered_df.empty:
-            total_notifications_mun_noti = filtered_df['notificacoes_total'].iloc[0]
-            selected_city_name = filtered_df['nome_noti'].iloc[0]  # Obtaining the selected city's name
-        else:
-            total_notifications_mun_noti = 0
-            selected_city_name = "Selected Municipality"
-
-        # Build the trace_mun_infe
         trace_mun_infe = go.Bar(
             y=dados_grafico_colunas['nome_infe'],
             x=dados_grafico_colunas['notifications'],
@@ -65,7 +48,6 @@ def update_graph_callback(selected_cidade, click_data, show_all_clicks, df, G, p
             name='mun_infe',
         )
 
-        # Atualize o título para incluir o nome do município selecionado
         grafico_colunas = {
             'data': [trace_mun_infe],
             'layout': {
@@ -73,15 +55,15 @@ def update_graph_callback(selected_cidade, click_data, show_all_clicks, df, G, p
                 'height': 700,
                 'width': 700,
                 'yaxis': {
-                    'tickmode': 'linear',  # Define o modo de ticks como linear
+                    'tickmode': 'linear',
                     'tickvals': dados_grafico_colunas['nome_infe'],
                     'ticktext': dados_grafico_colunas['nome_infe'],
-                    'dtick': 1,  # Ajusta o espaçamento entre os ticks
-                    'automargin': True,  # Ajusta automaticamente as margens para evitar cortes
+                    'dtick': 1,
+                    'automargin': True,
                 },
                 'xaxis': {'title': 'Notificações'},
-                'margin': {'l': 150},  # Aumenta a margem esquerda para acomodar os nomes
+                'margin': {'l': 150},
             }
         }
 
-    return grafo_direcional, grafico_colunas, selected_cidade, show_all_clicks
+    return grafo_direcional, grafico_colunas, selected_city, show_all_clicks
