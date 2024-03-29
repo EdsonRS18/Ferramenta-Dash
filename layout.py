cc46d3eca6c91054bfd95a7404d898cc7c1c150b
Edson Gomes <edson.bol.santos@hotmail.com>
2024-03-28 23:55:41 -0300

    adicionando imagens e mudando titulo

--- layout.py
+++ layout.py
@@ -3,7 +3,7 @@ import dash_bootstrap_components as dbc
 
 def create_layout(df):
     # Título e Subtítulo
-    header = html.H2('Dashboard de Malária', className='text-center mt-3 mb-4')
+    header = html.H2('Painel de Infecções e Notificações de Malária', className='text-center mt-3 mb-4')
     subheader = html.H4('', className='text-center mb-5')
 
     # Dropdown de Seleção de Cidade
@@ -20,13 +20,18 @@ def create_layout(df):
         style={'color': '#000'}  # Melhora a legibilidade do texto
     )
 
-    # Checklists e Botões
-    hide_matching_checkbox = dcc.Checklist(
-        id='hide-matching-municipality',
-        options=[{'label': 'Ocultar infecções ocorridas no próprio município', 'value': 'hide'}],
-        value=[],
-        className='mb-3'
-    )
+    hide_matching_checkbox = html.Div([
+    html.Label([
+        dcc.Checklist(
+            id='hide-matching-municipality',
+            options=[{'label': '   Ocultar infecções ocorridas no próprio município', 'value': 'hide'}],
+            value=[],
+            # Removido o style inline para ajuste via CSS
+            className='custom-checkbox-label',  # Classe customizada para estilizar especificamente este label
+        )
+    ], style={'display': 'block', 'margin-left': '20px'}),  # Adiciona um espaço à esquerda do checkbox
+], className='mb-3')
+
     update_button = dbc.Button(
         'Mostrar Todas as Cidades',
         id='update-button',
@@ -47,16 +52,62 @@ def create_layout(df):
         className='mb-3'
     )
 
-    # Gráficos
-    grafo_direcional = dcc.Graph(id='grafo-direcional', className='mb-3')
-    grafico_colunas = dcc.Graph(id='grafico-colunas', className='mb-3')
-    grafico_linha = dcc.Graph(id='line-chart', className='mb-3')
+    # Gráficos com Spinners
+    grafo_direcional = html.Div([
+        dcc.Loading(
+            id="loading-grafo-direcional",
+            type="circle",
+            children=dcc.Graph(id='grafo-direcional', className='mb-3')
+        )
+    ])
+    grafico_colunas = html.Div([
+        dcc.Loading(
+            id="loading-grafico-colunas",
+            type="circle",
+            children=dcc.Graph(id='grafico-colunas', className='mb-3')
+        )
+    ])
+    grafico_linha = html.Div([
+        dcc.Loading(
+            id="loading-grafico-linha",
+            type="circle",
+            children=dcc.Graph(id='line-chart', className='mb-3')
+        )
+    ])
 
-    # Organização do Layout com uma estética aprimorada
+    # Inserindo as imagens JPG e PNG
+
+    img_upe_url = "https://upload.wikimedia.org/wikipedia/commons/9/9b/Logo-upe-site.png"
+    img_dotlab_url = "https://avatars.githubusercontent.com/u/72280399?s=280&v=4"
+
+
+
+
+        # Cria as tags de imagem
+    img_jpg = html.Img(src=img_upe_url, className='img-fluid', style={'max-height': '100px', 'max-width': '100px'})
+    img_png = html.Img(src=img_dotlab_url, className='img-fluid', style={'max-height': '100px', 'max-width': '100px'})
+
+    # Coluna para as imagens com ajuste para centralização horizontal e vertical
+    images_column = dbc.Col(
+        html.Div(
+            [img_jpg, img_png],
+            className='d-flex justify-content-center align-items-center',  # Centraliza as imagens na coluna
+            style={'height': '100%'}  # Assegura que o div interno ocupe toda a altura da coluna, para o alinhamento vertical funcionar
+        ),
+        width=4,
+        className='d-flex justify-content-end align-items-start',  # Estes estilos podem ser ajustados ou removidos conforme o desejado
+    )
+    # Cria o layout da aplicação
+    # Atualiza a estrutura do layout para incluir a coluna de imagens atualizada
     layout = html.Div([
         dbc.Container([
-            header,
-            #subheader,
+            dbc.Row([
+                dbc.Col([header], width=8),  # Coluna para o título com 8 espaços
+                images_column,  # Coluna atualizada para as imagens
+        ], className='mb-5 align-items-center'),  # Alinhar verticalmente o conteúdo da linha ao centro # Alinhar verticalmente o conteúdo da linha ao centro
+            dbc.Row([
+                dbc.Col([], width=12),  # Coluna vazia para criar espaço
+            ], className='mb-2'),  # Definindo a margem apenas abaixo do título
             dbc.Row([
                 dbc.Col(cidade_dropdown, width=12, lg=4),
                 dbc.Col(hide_matching_checkbox, width=12, lg=4),
